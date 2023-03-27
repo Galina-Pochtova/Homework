dca5576bde96d4ff06f17b1ffb89883b14df54f6
Galina Pochtova <lina.pa1@mail.ru>
2023-03-27 20:51:07 +0500

    Task1

---
+++ Homework1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Tesk 1.  Напишите программу, которая на вход принимает два числа и выдаёт, 
+//какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3 
+
+/*Console.Write("Введите первое число: ");
+int a = Convert.ToInt32 (Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32 (Console.ReadLine());
+
+int max = int.MaxValue;
+int min = int.MinValue;
+
+if (a > b)
+{
+    Console.Write("Число" +  a  + "больше числа" + b );
+}
+else
+{
+    Console.Write("Число" + b + "больше числа" + a );
+}
+*/
+
+
+
+
+
