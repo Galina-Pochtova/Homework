eff840a6c5c46e161194ae3e34727584a1c59b2f
Galina Pochtova <lina.pa1@mail.ru>
2023-03-28 09:49:33 +0500

    Task

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -9,9 +9,6 @@ int a = Convert.ToInt32 (Console.ReadLine());
 Console.Write("Введите второе число: ");
 int b = Convert.ToInt32 (Console.ReadLine());
 
-int max = int.MaxValue;
-int min = int.MinValue;
-
 if (a > b)
 {
     Console.Write("Число" +  a  + "больше числа" + b );
@@ -22,7 +19,60 @@ else
 }
 */
 
+//Task 2. Напишите программу, которая принимает на вход три числа
+ //и выдаёт максимальное из этих чисел.
 
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
 
+/*Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
 
+int max = num1;
+if (num1> max) max = num1;
+if (num2> max) max = num2;
+if (num3> max) max = num3;
 
+Console.Write("max = ");
+Console.WriteLine(max);
+*/
+//Task 3.  Напишите программу, которая на вход принимает число
+//и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+//4 -> да
+//-3 -> нет
+//7 -> нет
+
+/*Console.Write("Введите число: ");
+double number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Число является четным. ");
+}
+else
+{
+    Console.WriteLine("Число не является четным. ");
+}
+*/
+
+//Task 4. Напишите программу, которая на вход принимает число (N),
+//а на выходе показывает все чётные числа от 1 до N.
+
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+/*Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int x = 2;
+
+while (x<= N)
+{
+    Console.Write( x );
+    x = x + 2;
+}
+*/
