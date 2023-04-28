4281c72ced1930a4357ac5722344e2d456ccc75b
Galina Pochtova <lina.pa1@mail.ru>
2023-04-28 13:48:50 +0500

    Homework8

---
+++ Homework8/Program.cs
@@ -0,0 +1,269 @@
+﻿// Задача 1. Задайте двумерный массив. Напишите программу, которая упорядочит по 
+//убыванию элементы каждой строки двумерного массива.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//В итоге получается вот такой массив:
+//7 4 2 1
+//9 5 3 2
+//8 4 4 2
+
+// int[,] CreateArray()
+// {
+//     Console.Write("Введите колличество строк: ");
+//     int rows = Convert.ToInt32(Console.ReadLine());
+//     Console.Write("Введите колличество столбцов: ");
+//     int columns = Convert.ToInt32(Console.ReadLine());
+//     Console.Write("Введите минимальное число: ");
+//     int min = Convert.ToInt32(Console.ReadLine());
+//     Console.Write("Введите максимальное число: ");
+//     int max = Convert.ToInt32(Console.ReadLine());
+//     int[,] array = new int[rows, columns];
+//     for(int i = 0; i < rows; i ++)
+//     {
+//         for(int j = 0; j < columns; j++)
+//         {
+//             array[i, j] = new Random().Next(min, max);
+//         }
+//     }
+//     return array;
+// }
+// void Show2dArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//              Console.Write(array[i,j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
+// void SortElements(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             for (int k = 0; k < array.GetLength(1) - 1; k++)
+//             {
+//                 if (array[i, k] < array[i, k + 1])
+//                 {
+//                     int temp = array[i, k + 1];
+//                     array[i, k + 1] = array[i, k];
+//                     array[i, k] = temp;
+//                 }
+//             }
+//         }
+//     }
+// }
+// int[,] copyArray = CreateArray();
+// Show2dArray(copyArray);
+// SortElements(copyArray);
+// Show2dArray(copyArray);
+
+
+//Задача 2. Задайте прямоугольный двумерный массив. Напишите программу, 
+//которая будет находить строку с наименьшей суммой элементов.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//5 2 6 7
+//Программа считает сумму элементов в каждой строке и выдаёт номер строки с 
+//наименьшей суммой элементов: 1 строка
+
+// int[,] CreateRandom2DArray()
+// {
+//     Console.Write("Введите количество строк: ");
+//     int rows = Convert.ToInt32(Console.ReadLine());
+//     Console.Write("Введите количество столбцов: ");
+//     int columns = Convert.ToInt32(Console.ReadLine());
+//     Console.Write("Введите минимальное значение: ");
+//     int minValue = Convert.ToInt32(Console.ReadLine());
+//     Console.Write("Введите максимальное значение: ");
+//     int maxValue = Convert.ToInt32(Console.ReadLine());
+
+//     int[,] array = new int[rows, columns];
+
+//     for(int i = 0; i < rows; i++)
+//         for(int j = 0; j < columns; j++)
+//             array[i,j] = new Random().Next(minValue, maxValue+ 1);
+
+//     return array;
+// }
+
+// void Show2DArray(int[,] array)
+// {
+//     for(int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for(int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i,j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
+
+// void MinSumRow(int[,] array)
+// {
+//     int minValueRow = 0;
+//        for (int i = 0; i < array.GetLength(1); i++)
+//     {
+//         minValueRow += array[0, i];
+//     }
+    
+//     int minnumberSum = 0;
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         int sumRow = 0;
+//         for (int j = 0; j < array.GetLength(1); j++) sumRow += array[i, j];
+//         if (sumRow < minValueRow)
+//         {
+//             minValueRow = sumRow;
+//             minnumberSum = i;
+//         }
+//         sumRow = 0;
+//     }
+//     Console.Write($"Номeр строки с наименьшей суммой: {minnumberSum +1} ");
+// }
+
+// int[,] myArray = CreateRandom2DArray();
+// Show2DArray(myArray);
+// MinSumRow(myArray);
+
+
+
+//Задача 3. Задайте две матрицы. Напишите программу, которая будет 
+//находить произведение двух матриц.
+//Например, даны 2 матрицы:
+//2 4 | 3 4
+//3 2 | 3 3
+//Результирующая матрица будет:
+//18 20
+//15 18
+
+// Console.WriteLine("Введите размеры матриц и диапазон случайных значений:");
+// int m = InputNumbers("Введите число строк 1-й матрицы: ");
+// int n = InputNumbers("Введите число столбцов 1-й матрицы (и строк 2-й): ");
+// int p = InputNumbers("Введите число столбцов 2-й матрицы: ");
+// int range = InputNumbers("Введите диапазон случайных чисел: от 1 до ");
+
+// int[,] firstMartrix = new int[m, n];
+// CreateArray(firstMartrix);
+// Console.WriteLine($"Первая матрица:");
+// WriteArray(firstMartrix);
+
+// int[,] secomdMartrix = new int[n, p];
+// CreateArray(secomdMartrix);
+// Console.WriteLine($"Вторая матрица:");
+// WriteArray(secomdMartrix);
+
+// int[,] resultMatrix = new int[m,p];
+
+// MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
+// Console.WriteLine($"Произведение первой и второй матриц:");
+// WriteArray(resultMatrix);
+
+// void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+// {
+//   for (int i = 0; i < resultMatrix.GetLength(0); i++)
+//   {
+//     for (int j = 0; j < resultMatrix.GetLength(1); j++)
+//     {
+//       int sum = 0;
+//       for (int k = 0; k < firstMartrix.GetLength(1); k++)
+//       {
+//         sum += firstMartrix[i,k] * secomdMartrix[k,j];
+//       }
+//       resultMatrix[i,j] = sum;
+//     }
+//   }
+// }
+
+// int InputNumbers(string input)
+// {
+//   Console.Write(input);
+//   int output = Convert.ToInt32(Console.ReadLine());
+//   return output;
+// }
+
+// void CreateArray(int[,] array)
+// {
+//   for (int i = 0; i < array.GetLength(0); i++)
+//   {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//       array[i, j] = new Random().Next(range);
+//     }
+//   }
+// }
+
+// void WriteArray (int[,] array)
+// {
+//   for (int i = 0; i < array.GetLength(0); i++)
+//   {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//       Console.Write(array[i,j] + " ");
+//     }
+//     Console.WriteLine();
+//   }
+// }
+
+
+
+//Задача 4. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+//Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+//Массив размером 2 x 2 x 2
+//66(0,0,0) 25(0,1,0)
+//34(1,0,0) 41(1,1,0)
+//27(0,0,1) 90(0,1,1)
+//26(1,0,1) 55(1,1,1)
+
+// int[,,] CreateRandom3DArray()
+// {
+//     Console.Write("Введите длину массив, кол-во элементов по X: ");
+//     int x = Convert.ToInt32(Console.ReadLine());
+//     Console.Write("Введите ширину массива, кол-во элементов по Y: ");
+//     int y = Convert.ToInt32(Console.ReadLine());
+//     Console.Write("Введите высоту массива, кол-во элементов по Z: ");
+//     int z = Convert.ToInt32(Console.ReadLine());
+       
+//     int[,,] array = new int[x, y, z];
+
+//     for(int i = 0; i < array.GetLength(0); i++)
+//         for(int j = 0; j < array.GetLength(1); j++)
+//             for(int k = 0; k < array.GetLength(2); k++)  
+//                 array[i,j,k] = new Random().Next(10, 100);
+//     return array;
+// }
+
+// void Show3DArray(int[,,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.WriteLine();
+//             for (int k = 0; k < array.GetLength(2); k++)
+//             {
+//                 Console.Write($"{array[i, j, k]}({i},{j},{k}) ");
+//             }
+//         }
+//     }
+// }
+
+// int[,,] newArray = CreateRandom3DArray();
+// Show3DArray(newArray);
+
+
+//Задача 5. Напишите программу, которая заполнит спирально массив 4 на 4.
+//Например, на выходе получается вот такой массив:
+//01 02 03 04
+//12 13 14 05
+//11 16 15 06
+//10 09 08 07
